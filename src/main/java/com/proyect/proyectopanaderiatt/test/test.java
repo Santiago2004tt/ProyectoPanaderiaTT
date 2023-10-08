8889f397991c9ca842cd5cad56b51cd305e23362
Santiago2004tt <115126538+Santiago2004tt@users.noreply.github.com>
2023-10-08 16:48:17 -0500

    Correcion
    Se corrigio el error del module-info

--- src/main/java/com/proyect/proyectopanaderiatt/test/test.java
+++ src/main/java/com/proyect/proyectopanaderiatt/test/test.java
@@ -2,6 +2,6 @@ package com.proyect.proyectopanaderiatt.test;
 
 public class test {
     public static void main(String[] args) {
-
+        System.out.println("hola");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.proyect.proyectopanaderiatt {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.proyect.proyectopanaderiatt to javafx.fxml;
     exports com.proyect.proyectopanaderiatt.Application;
     opens com.proyect.proyectopanaderiatt.Application to javafx.fxml;
     exports com.proyect.proyectopanaderiatt.controllers;
